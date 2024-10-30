dac8ac031bf87c38d089743d160f3a05144f5ff6
nutterthanos <minecraftitsover90@gmail.com>
2024-10-30 08:54:50 +0000

    Update grab_mayhemids.py

--- grab_mayhemids.py
+++ grab_mayhemids.py
@@ -7,8 +7,8 @@ from aiohttp import ClientResponseError, ClientPayloadError, ClientConnectorErro
 from xml.etree import ElementTree as ET
 
 # Define the range for applicationUserId
-START_ID = 142000000
-END_ID = 143000000
+START_ID = 2000000000
+END_ID = 201000000
 
 # Maximum concurrent requests and retries
 MAX_CONCURRENT_REQUESTS = 25000
